d9b0160bfa105006b2bf610ad4223986acfd8ce2
Sanya Anik <anikanov.alexander@gmail.com>
2025-05-21 18:04:49 +0300

    crud working

--- src/main/java/com/anikanov02/selfhost/domain/dto/message/MessagesPaginatedRequest.java
+++ src/main/java/com/anikanov02/selfhost/domain/dto/message/MessagesPaginatedRequest.java
@@ -5,10 +5,16 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.UUID;
 
 @Getter
 @Setter
 public class MessagesPaginatedRequest {
+    @NotNull(message = "Chat id is required")
+    private UUID chatId;
+
     @NotNull(message = "Page is required")
     @Min(value = 1, message = "Page number must be greater than or equal to 1")
     private Integer page;
